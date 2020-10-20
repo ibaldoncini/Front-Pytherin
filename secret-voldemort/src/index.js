9c5de0b726b58c90246549ade6bbbf3c63d53897
= <airascaesteban@gmail.com>
2020-10-20 19:59:37 -0300

    Finished register page, form action and index page to define but its now working

--- secret-voldemort/src/index.js
+++ secret-voldemort/src/index.js
@@ -1,13 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import { Provider } from "react-redux";
-
-import Register from "./registerPage";
+import Register from "./registerPage"
 
 const Root = (
-      <Register />
+      <Router>
+        <Switch>
+          <Route path='/registerPage' exact render={() => <Register/>}/>
+          <Route path='/index' exact />
+        </Switch>
+      </Router>
 )
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(Root,rootElement);
+const rootElement = document.getElementById("root")
+ReactDOM.render(Root,rootElement)
--- secret-voldemort/src/registerPage.js
+++ secret-voldemort/src/registerPage.js
@@ -1,34 +1,47 @@
 import React from "react";
-//import Email from "./components/email.js"
-//import ButtonsRegister from "./components/bRegister"
-//  va abajo  <Email /> <ButtonsRegister />
+import { Redirect } from "react-router-dom"
 export default class Register extends React.Component{
     constructor(props){
         super(props);
 
-        this.state = {nameUser:'',
-                      passUser: '',
-                      passUser2: '',
-                      mailUser: '',
-                      img: React.createRef()};
+        this.state = {
+                    nameUser:'',
+                    passUser: '',
+                    passUser2: '',
+                    mailUser: '',
+                    redir: false,
+                    toPage: '',
+                    img: React.createRef()
+                    };
         // Event handlers
         this.handleChange = this.handleChange.bind(this)
         this.handleChangeImg = this.handleChangeImg.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.cleanFile = this.cleanFile.bind(this)
         this.handleRedirect = this.handleRedirect.bind(this)
     }
+    // methods
     handleChange(e) {
        this.setState({[e.target.name]: e.target.value})
       }
 
     handleChangeImg(e){
-        this.setState({img: e.target.files[0]});
+        this.setState({img: e.target.files[0]})
     }
+
+    cleanFile(){ // take care with null and object or file object
+        document.getElementById("logoUser").value = null
+        this.setState({img: null})
+    }
+    // validation
     handleSubmit(e){
         var regExpMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
         var regExpPsw = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$/
         if(this.state.nameUser === ""){
             alert("Deberá seleccionar un nombre.")
+            e.preventDefault();
+        }else if(this.state.nameUser.length < 8){
+            alert("El nombre de usuario deberá ser de al menos 8 caracteres.")
             e.preventDefault();
         }else if(this.state.passUser === ""){
             alert("Deberá seleccionar una contraseña.")
@@ -45,29 +58,27 @@ export default class Register extends React.Component{
         } else if(this.state.mailUser === "" || !regExpMail.test(this.state.mailUser)){
             alert("Deberá seleccionar un e-mail válido.")
             e.preventDefault();
-        }else if(this.state.img === null){
-            alert("Deberá seleccionar una imagen de perfil.")
-            e.preventDefault();
-        }else{
-            alert(this.state.img.toString())
-            alert("Form submited!")
-            e.preventDefault()
         }
     }
-        handleRedirect(e){
-            alert("Redirected!")
-        }
+    handleRedirect(){
+        this.setState({redir: true})
+        this.setState({toPage : '/index'})
+    }
     render(){
-           return <div className="registerUser">
-                <form name='fRegister' onSubmit={this.handleSubmit}>
+            if(this.state.redir){
+                return <Redirect to={this.state.toPage} />    
+            }
+            return <div className="registerUser">
+                <form action='users/register' method='POST' id='fRegister' name='fRegister' onSubmit={this.handleSubmit}>
                     <h1>Registrar nuevo usuario</h1>
                     <label>Nombre: </label><input type="text" name="nameUser" id="nameUser" value={this.state.nameUser} onChange={this.handleChange}></input> <br/>
                     <label>Contraseña: </label><input type="password" name="passUser" id="passUser" value={this.state.passUser} onChange={this.handleChange}></input><br/>
                     <label>Repita contraseña: </label><input type="password" name="passUser2" id="passUser2" value={this.state.passUser2} onChange={this.handleChange}></input><br/>
                     <label>E-mail: </label><input type="text" name="mailUser" id="mailUser" value={this.state.mailUser} onChange={this.handleChange}></input><br/>
-                    <label>Imagen: </label><input type="file" ref={this.img} name="logoUser" id="logoUser" onChange={this.handleChangeImg}></input><br/>
-                    <input type="submit" id="regBtn" name="regBtn"/>
-                    <input type="button" id="cancelBtn" name="cancelBtn" value="Cancelar" onClick={this.handleRedirect}/>
+                    <label>Imagen: </label><input type="file" ref={this.img} name="logoUser" id="logoUser" onChange={this.handleChangeImg}></input>
+                    <input type="button" id="cleanBtn" name="cleanBtn" value="Limpiar" onClick={this.cleanFile}/><br/>
+                    <input type="submit" id="regBtn" name="regBtn" value="Crear cuenta"/>
+                    <input type="button" id="cancelBtn" name="cancelBtn" onClick={this.handleRedirect} value="Cancelar"/>
                 </form>
             </div>;
         }
