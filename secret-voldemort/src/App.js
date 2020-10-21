d5f1f7213e5c14226d9526582a219ff0b2b0cace
Marian00P <mpaludi00@gmail.com>
2020-10-20 23:35:18 -0300

    Generalized button component created.

--- secret-voldemort/src/App.js
+++ secret-voldemort/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import FormCreateRoom from './components/CreateRoom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Button from './components/Button';
+
 
 function App() {
+
   return (
-    <div className="App">
+    <BrowserRouter>
 
-      <body>
-        <FormCreateRoom />
-      </body>
+          <Route exact path="/createRoom">
+            <FormCreateRoom />
+          </Route>
 
-    </div>
+          <Route exact path="/">
+            <div> 
+              <header> Bienvenido a Secret-Voldemort </header>
+              <Button path="/createRoom" text="Create a new room"/>
+            </div>
+          </Route>
+
+    </BrowserRouter>
   );
 }
 
--- secret-voldemort/src/components/CreateRoom.js
+++ secret-voldemort/src/components/CreateRoom.js
@@ -1,18 +1,5 @@
 import React from 'react';
 
-
-/* This function generates the button 
-that displays the form to create the room. 
-¡¡¡[Does not work]!!! */
-
-function RoomCreationButton() {
-    return (
-        <div className="RCButton">
-          <button onClick={FormCreateRoom}>Create a room</button>
-        </div>
-    )
-} // export default RoomCreationButton;
-
 /* This component is in charge of collecting the 
 data entered by the user and sending it to the corresponding endpoint. */
 
@@ -23,53 +10,60 @@ class FormCreateRoom extends React.Component {
 
     this.state = {
       room_name: '',
-      no_players: 5
+      max_players: 5
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChangeNoPlayers = this.handleChangeNoPlayers.bind(this);
+    this.handleChangeMaxPlayers = this.handleChangeMaxPlayers.bind(this);
     this.handleChangeRoomName = this.handleChangeRoomName.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    const {room_name, no_players} = this.state;
-    if (no_players && room_name) {
+    const {room_name, max_players} = this.state;
+    if (max_players && room_name) {
       alert('Creating room ' + room_name)
     } else {
       alert('Please fill in all fields correctly.')
     }
   }
   
-  handleChangeNoPlayers(event) {
+  handleChangeMaxPlayers(event) {
     const value = event.target.value;
     this.setState({
-      no_players: value
+      max_players: value
     })
   }
 
   handleChangeRoomName(event) {
-    const value = event.target.value;
+    let value = event.target.value;
+    value = value.replace(/[^A-Za-z1-9]/gi, "");
+    
     this.setState({
       room_name: value
     })
   }
 
   render() {
-    const  {room_name, no_players} = this.state;
     return (
         <div className="FormCreateRoom">
             <h2>Creation of room</h2>
             <form name="form" onSubmit={this.handleSubmit}>
                 <div className="Room_name">
-                    <label htmlFor="room_name">Room name </label>
-                    <input type="text" value={room_name} onChange={this.handleChangeRoomName}  name="roomName" />
+                    <label>Room name <br/>
+                      <input type="text" maxLength='30' minLength='3'
+                        value={this.state.room_name} min='5' max='10'
+                        onChange={this.handleChangeRoomName} name="roomName"/>
+                    </label> <br/>
                 </div>
-                <div className="Number_of_Players">
-                    <label htmlFor="no_players">Number of players </label>
-                    <input type="number" value={no_players} min='5' max='10' onChange={this.handleChangeNoPlayers}  name="noPlayers" />
-                </div>
-                <button onSubmit={this.handleSubmit}>Create room</button>
+                <div className="Max_Players">
+                    <label>Maximum number of players <br/>
+                      <input type="number" value={this.state.max_players} 
+                        min='5' max='10' onChange={this.handleChangeMaxPlayers} 
+                        name="maxPlayers" />
+                    </label> <br/>
+                </div> <br/>
+                <input type='submit' value='Create room'/>
             </form>
         </div>
     );
