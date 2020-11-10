282845b4ce144158b4a6f04f2ee316b48f093545
jerober <jerobernardi@gmail.com>
2020-11-10 15:54:33 -0300

    Testing Done, some issue remain to PR

--- secret-voldemort/src/components/RoomTag.js
+++ secret-voldemort/src/components/RoomTag.js
@@ -8,25 +8,29 @@ export const RoomTag = props =>{
   const rootpath = '/joinRoom/'
   
   return (
-    <div class='container my-2 room-row'>
-      {
-        room === '' 
-        ?
-        <div class='columns py-3 is-vcentered align-cntr'>
-          <div class='column is-6'>
-            {props.room.name}
-          </div>
-          <div class='column is-2'>
-            {props.room.active_users}/{props.room.max_players}
-          </div>
-          <div class='column is-4'>
-            <button class='room-button' onClick={() => {chooseRoom(props.room.name)}}>Unirse</button>
-          </div>
-        </div>
-        :
-        <Redirect to={rootpath+room}/>
-      }
+    (props.room.active_users !== props.room.max_players) 
+    ?
+      <div class='container my-2 room-row'>
+        {
+          (room === '' 
+          ?
+            <div id={props.room.name} class='columns py-3 is-vcentered align-cntr'>
+              <div id='name' class='column is-6'>
+                {props.room.name}
+              </div>
+              <div id='#players' class='column is-2'>
+                {props.room.active_users}/{props.room.max_players}
+              </div>
+              <div class='column is-4'>
+                <button id={`${props.room.name}join`} class='room-button' onClick={() => {chooseRoom(props.room.name)}}>Unirse</button>
+              </div>
+            </div>
+          :
+          <Redirect to={rootpath+room}/>)
+        }
+      </div>
+    :
+      <div></div>
       
-    </div>
   );
 }
