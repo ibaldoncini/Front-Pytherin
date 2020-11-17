0b184d05485f0a0928b13173b65067259cae85c7
= <airascaesteban@gmail.com>
2020-11-16 23:54:06 -0300

    Test login and register now working

--- secret-voldemort/cypress/integration/login.spec.js
+++ secret-voldemort/cypress/integration/login.spec.js
@@ -2,22 +2,20 @@
   To be able to run these tests it is necessary to install certain dependencies: 
       npm install cypress --save-dev 
    To run these tests use the command and select the test: 
-      npm run test:login
+      npm run test:integration
 */
 context('User login', () => {
     describe('Accessing the login page', () => {
         it("Try to access the login page it should display an h1", () => {
             cy.visit("http://localhost:3000")
             cy.get("h1").contains("Pytherin Project")
-            cy.get("h2").containt("To play you need to be logged in")
+            cy.get("h2").contains("To play you need to be logged in")
         })
     })
 
     describe('Login without fill fields', () => {
         it("Try to login without fill any field, shouldn't login", () => {
             cy.visit("http://localhost:3000")
-            cy.get("#inemail").type("player0@example.com")
-            cy.get("#inpsw").type("Heladera65")
             cy.get("input[type=submit]").contains("Login")
             .click()
             cy.get("#allEmptyValid").contains("* There are empty fields")
@@ -77,8 +75,8 @@ context('User login', () => {
             cy.get('h1').should("be.visible")
             cy.get('#create').should("be.visible").and("contain", "Create room")
             cy.get('#join').should("be.visible").and("contain", "Join room")
-            cy.get('#change').should("be.visible").and("contain", "Change nickname")
-            cy.get('#profile').should("be.visible").and("contain", "Profile")
+            cy.get('#change_nickname').should("be.visible").and("contain", "Change nickname")
+            cy.get('#change_password').should("be.visible").and("contain", "Change password")
             cy.get('#logout').should("be.visible").and("contain", "Logout")
         })
     })
